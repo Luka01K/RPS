963bd578373be4431e1cf3c16dd20e7b390371e1
Luka01K <147425539+Luka01K@users.noreply.github.com>
2024-02-27 19:38:14 +0100

    implementing JS
    updating the html, adding buttons and displaying
    the results, choices and num of rounds

--- javascript.js
+++ javascript.js
@@ -1,6 +1,47 @@
-//Make a function that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. 
-//
 
+
+const playerText = document.querySelector('.playerText');
+const computerText = document.querySelector('.computerText');
+const resultText = document.querySelector('.resultText');
+const choiceBtns = document.querySelectorAll('.choiceBtn');
+const roundCounter = document.querySelector('.roundCounter');
+
+let playerSelection;
+let computerSelection;
+let result;
+let numberOfRounds = 0;
+let playerPoints = 0;
+let computerPoints = 0;
+let drawPoints = 0;
+
+
+/* function activates when a button is clicked, updates the results*/
+choiceBtns.forEach(button => button.addEventListener('click', () => {
+
+    if (playerPoints || computerPoints < 5) {
+        playerSelection = button.textContent;
+        computerSelection = getComputerChoice();
+        numberOfRounds++;
+
+    /* console.log(playerSelection);
+        console.log(computerSelection); */
+    
+        playerText.textContent = `Player: ${playerSelection}`;
+        computerText.textContent = `Computer: ${computerSelection}`;
+        resultText.textContent = "Result: " + playRound(playerSelection, computerSelection);
+        roundCounter.textContent = `Number of rounds played: ${numberOfRounds}`
+    }
+}))
+
+
+
+
+
+
+
+
+
+/* function generates a random number which determines computers choice */
 function getComputerChoice() {
 
     let computerNumber = Math.floor(Math.random() * 3) + 1
@@ -15,63 +56,20 @@ function getComputerChoice() {
 
 };
 
+
+/* function compares players choice with computers choice and returns the outcome */
 function playRound(playerSelection, computerSelection) {
 
-    if (playerSelection === "Rock" && computerSelection === "Rock" || playerSelection === "Paper" && computerSelection === "Paper" || playerSelection === "Scissors" && computerSelection === "Scissors") {
+    if (playerSelection == "Rock" && computerSelection == "Rock" || playerSelection == "Paper" && computerSelection == "Paper" || playerSelection == "Scissors" && computerSelection == "Scissors") {
         return "Its a draw. Nobody gets points."
 
-    } else if (playerSelection === "Rock" && computerSelection === "Scissors" || playerSelection === "Paper" && computerSelection === "Rock" || playerSelection === "Scissors" && computerSelection === "Paper") {
+    } else if (playerSelection == "Rock" && computerSelection == "Scissors" || playerSelection == "Paper" && computerSelection == "Rock" || playerSelection == "Scissors" && computerSelection == "Paper") {
         return "You win! You also get a point."
 
-    } else if (playerSelection === "Rock" && computerSelection === "Paper" || playerSelection === "Paper" && computerSelection === "Scissors" || playerSelection === "Scissors" && computerSelection === "Rock") {
+    } else if (playerSelection == "Rock" && computerSelection == "Paper" || playerSelection == "Paper" && computerSelection == "Scissors" || playerSelection == "Scissors" && computerSelection == "Rock") {
         return "You loose! Computer gets a point, you get none."
 
     } else {
         return "The option you choose is invalid. You can only choose between: Rock, Paper and Scissors. Try again."
     }
 };
-
-
-let playerPoints = 0;
-let computerPoints = 0;
-let drawPoints = 0;
-let numberOfRounds = 0;
-
-function playGame() {
-
-    while (numberOfRounds < 5) {
-        
-        let playerSelectionMessy = prompt("Whats you'r weapon of choice?");
-        let playerSelection = playerSelectionMessy.toLowerCase().charAt(0).toUpperCase() + playerSelectionMessy.slice(1).toLowerCase();
-        
-        let computerSelection = getComputerChoice();
-        console.log("Computer chose:", computerSelection);
-        console.log("You chose:", playerSelection);
-        console.log(playRound(playerSelection, computerSelection));
-
-        if (playRound(playerSelection, computerSelection) === "You win! You also get a point.") {
-            playerPoints++;
-            numberOfRounds++;
-            console.log("Your points:", playerPoints);
-            console.log("Computer points:", computerPoints);
-            console.log("Games played:", numberOfRounds);
-        } else if (playRound(playerSelection, computerSelection) === "You loose! Computer gets a point, you get none.") {
-            computerPoints++;
-            numberOfRounds++;
-            console.log("Your points:", playerPoints);
-            console.log("Computer points:", computerPoints);
-            console.log("Games played:", numberOfRounds);
-        } else {
-            numberOfRounds++;
-            drawPoints++;
-            console.log("Number of draws:", drawPoints);
-            console.log("Your points:", playerPoints);
-            console.log("Computer points:", computerPoints);
-            console.log("Games played:", numberOfRounds);
-        }
-    }
-
-    console.log("Final score:   " + " " + "Your points:", playerPoints + " " + "Computer points:", computerPoints + " " + "Number of draws:", drawPoints);
-}
-
-playGame()
